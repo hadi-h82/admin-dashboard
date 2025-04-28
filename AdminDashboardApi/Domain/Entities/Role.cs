dab7df46c500f7c29c87fa927851647134e40847
hadi <hadi.deris.h82@gmail.com>
2025-04-28 17:48:07 +0330

    Add basic entities: User, Role, Permission

---
+++ AdminDashboardApi/Domain/Entities/Role.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace AdminDashboardApi.Domain.Entities
+{
+    public class Role
+    {
+
+        public int Id { get; set; }
+        public required string Name { get; set; } = string.Empty;
+        public List<Permission> Permissions { get; set; } = new();
+
+
+    }
+
+
+    
+}
