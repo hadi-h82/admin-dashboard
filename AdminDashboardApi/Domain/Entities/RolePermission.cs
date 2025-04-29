cb5ba8b9a1a6070a447a5e934b7d4e1e27ac2138
hadi <hadi.deris.h82@gmail.com>
2025-04-29 14:53:45 +0330

    Add UserRole and RolePermission join entities

---
+++ AdminDashboardApi/Domain/Entities/RolePermission.cs
@@ -0,0 +1,11 @@
+﻿namespace AdminDashboardApi.Domain.Entities
+{
+    public class RolePermission
+    {
+        public Guid RoleId { get; set; }
+        public Role Role { get; set; } = default!;
+
+        public Guid PermissionId { get; set; }
+        public Permission Permission { get; set; } = default!;
+    }
+}
