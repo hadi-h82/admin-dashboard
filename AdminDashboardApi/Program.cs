02ff611d182dc39afe17ee92f03c6b75ea6abd9a
hadi <hadi.deris.h82@gmail.com>
2025-04-29 17:16:16 +0330

    edit Program.cs
    update launchSettings

--- AdminDashboardApi/Program.cs
+++ AdminDashboardApi/Program.cs
@@ -7,8 +7,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 
 // Add services to the container.
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
