63975735dc2ca34c0f00011de7af37b1d6c377d6
hadi <hadi.deris.h82@gmail.com>
2025-04-29 16:44:20 +0330

    edit Program.cs

--- AdminDashboardApi/Program.cs
+++ AdminDashboardApi/Program.cs
@@ -1,4 +1,6 @@
+using AdminDashboardApi.Application.Interfaces;
 using AdminDashboardApi.Infrastructure.Data;
+using AdminDashboardApi.Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +11,15 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+// Register Repositories
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
